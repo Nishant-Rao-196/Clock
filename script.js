9a2ab76522915aebd750071e10a5bf371ffee56e
Nishant Kumar <nishantrajyadav196@gmail.com>
2026-04-24 22:46:21 +0530

    Reformat clock script for consistency

--- script.js
+++ script.js
@@ -7,18 +7,15 @@ function runClock() {
 
     const sec = now.getSeconds();
     const min = now.getMinutes();
-    const hr  = now.getHours() % 12;
+    const hr = now.getHours() % 12;
 
     const secDeg = sec * 6;
     const minDeg = min * 6 + sec * 0.1;
-    const hrDeg  = hr * 30 + min * 0.5;
+    const hrDeg = hr * 30 + min * 0.5;
 
-    secondHand.style.transform =
-        `translateX(-50%) rotate(${secDeg}deg)`;
-    minuteHand.style.transform =
-        `translateX(-50%) rotate(${minDeg}deg)`;
-    hourHand.style.transform =
-        `translateX(-50%) rotate(${hrDeg}deg)`;
+    secondHand.style.transform = `translateX(-50%) rotate(${secDeg}deg)`;
+    minuteHand.style.transform = `translateX(-50%) rotate(${minDeg}deg)`;
+    hourHand.style.transform = `translateX(-50%) rotate(${hrDeg}deg)`;
 }
 
 setInterval(runClock, 1000);
